74b132afd4d4a4551d2760a0de1caea63ef925cd
sysadm <sysadm>
2016-12-04 07:35:06 +0100

    Add timer component

--- app/components/timer.jsx
+++ app/components/timer.jsx
@@ -1,11 +1,56 @@
 var React = require('react');
+var Clock = require('Clock');
+var Controls = require('Controls');
 
-var Timer = React.createClass({
-    render:function () {
+var CountDown = React.createClass({
+    getInitialState: function () {
+        return {
+            count: 0,
+            timerStatus: 'stopped'
+        };
+    },
+    componentWillUnmount: function () {
+        clearInterval(this.timer);
+        this.timer = undefined;
+    },
+    componentDidUpdate: function (prevProps, prevState) {
+        if (this.state.timerStatus !== prevState.timerStatus) {
+            switch (this.state.timerStatus) {
+                case 'started':
+                    this.handleStart();
+                    break;
+                case 'stopped':
+                    // reset
+                    this.setState({count: 0});
+                case 'paused':
+                    clearInterval(this.timer);
+                    this.time = undefined;
+                    break;
+            }
+        }
+    },
+    handleStart: function () {
+        this.timer = setInterval(() => {
+            this.setState({
+                count: this.state.count + 1
+            });
+
+        }, 1000);
+    },
+    handleStatusChange: function (newStatus) {
+        this.setState({timerStatus: newStatus});
+    },
+    render: function () {
+        var {count, timerStatus} = this.state;
+
         return (
-          <h5>Timer component</h5>
+            <div>
+                <h1 className="page-title">Timer App</h1>
+                <Clock totalSeconds={count}/>
+                <Controls countDownStatus={timerStatus} onStatusChange={this.handleStatusChange}/>
+            </div>
         );
     }
 });
 
-module.exports = Timer;
+module.exports = CountDown;
